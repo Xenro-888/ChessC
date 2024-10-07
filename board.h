bdde82fc48871d1755ce7e18bb434229a42f0162
Xenro-888 <DaygoSimmons@gmail.com>
2024-10-07 17:42:05 -0500

    Testing

--- board.h
+++ board.h
@@ -6,4 +6,3 @@ int is_path_blocked(piece* board[8][8], piece* piece_to_move, int file_increment
 int is_in_check(piece_color color, piece* white_pieces[16], piece* black_pieces[16], piece* board[8][8]);
 int is_move_valid_basic(piece* piece_to_move, int file, int rank, piece* board[8][8]);
 int process_move(piece* piece_to_move, int file, int rank, piece* white_pieces[16], piece* black_pieces[16], piece* board[8][8]);
-void display_board(piece* board[8][8], piece* piece_to_move, piece* white_pieces[16], piece* black_pieces[16]);
